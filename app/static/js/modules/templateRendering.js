4195dcdba7c97a27d4146b3a018940023ffbadf3
Sam-Guliker <samguliker@hotmail.com>
2018-05-16 11:41:31 +0200

    testing

--- app/static/js/modules/templateRendering.js
+++ app/static/js/modules/templateRendering.js
@@ -1,13 +1,11 @@
-// import
 import htmlElements from './createElements.js'
+import collection from './collection.js'
 
-// rendering the webpages
 var template = {
 
   overviewRender(data) {
     var source = document.getElementById("trending-template").innerHTML;
     var template = Handlebars.compile(source);
-    // Maakt de template altijd schoon.
     htmlElements.ul.innerHTML = ''
 
     htmlElements.body.appendChild(htmlElements.ul)
@@ -31,14 +29,8 @@ var template = {
     htmlElements.body.appendChild(htmlElements.ul)
     htmlElements.h1.innerHTML = "Random Gifs"
 
-    // insert the object into the html
     var html = template(data)
     htmlElements.ul.innerHTML = html
-    // toggle(route) {
-    //   // 2 show active route
-    //   settings.sections.forEach(function(el){
-    //     '#' + el.id === route ? el.classList.add('active') : el.classList.remove('active')
-    //   })
   },
 
   apiNotFound() {
